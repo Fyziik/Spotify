bafd99c68f708bc2579cd41e015035c2a93d2765
Andreas Møller Laursen <Fyziikme@gmail.com>
2022-09-04 19:10:02 +0200

    DB prework

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://Fyziik:<password>@cluster0.n60inow.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
